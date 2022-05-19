0e62f1388f4cf743ca840eb5a209f38b64b7133f
Gustavo <gust.bersan@gmail.com>
2022-05-19 19:16:39 -0300

    Primeiro commit

---
+++ TransferBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransferBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
