d16a52cda39e18681852968289e5d49a96f317c5
Gustavo <gust.bersan@gmail.com>
2022-05-25 21:27:51 -0300

    Criando classe (Conta) e instânciando atributos

---
+++ TransferBank/src/Classes/Conta.cs
@@ -0,0 +1,11 @@
+namespace TransferBank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Carteira { get; set; }
+        private string Nome { get; set; }
+
+    }
+}
