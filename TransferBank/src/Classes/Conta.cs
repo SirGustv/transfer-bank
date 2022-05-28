1d1d9f215199b00c69446c2d34737dbdc88b5ef6
Gustavo <gust.bersan@gmail.com>
2022-05-27 23:50:28 -0300

    Criando ação Sacar

--- TransferBank/src/Classes/Conta.cs
+++ TransferBank/src/Classes/Conta.cs
@@ -4,15 +4,30 @@ namespace TransferBank
     {
         private TipoConta TipoConta { get; set; }
         private double Saldo { get; set; }
-        private double Carteira { get; set; }
+        private double Credito { get; set; }
         private string Nome { get; set; }
 
         public Conta(TipoConta tipoConta, double saldo, double carteira, string nome)
         {
             this.TipoConta = tipoconta;
             this.Saldo = saldo;
-            this.Carteira = carteira;
+            this.Credito = carteira;
             this.Nome = nome;
+        }
+
+        public bool Sacar (double valorSaque)
+        {
+            if (this.Saldo - valorSaque < this.Credito *-1 )
+            {
+                Console.Writeline("Saldo insuficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.Writeline("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+            return true;
         }
     }
 }
